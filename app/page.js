03581a5c8dc772331e14624de4698608646c9f3d
SahilPanigrahi <119133054+SahilPanigrahi@users.noreply.github.com>
2024-03-26 15:04:01 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-8">
       <h1 className="gradient-title font-extrabold text-3xl md:text-6xl lg:text-8xl tracking-tighter md:px-6 text-center">
-        Thief Detection Alarm
+        Object Detector(Thief Detction Alarm)
       </h1>
       <ObjectDetection />
     </main>
